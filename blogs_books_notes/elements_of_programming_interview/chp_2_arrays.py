c224bcb2f6472391fa204f7e60ab1071363f17b6
benjaminbang987 <30059500+benjaminbang987@users.noreply.github.com>
2019-07-16 20:42:45 -0700

    Airflow celery update (#12)
    * adding notes for books, etc
    
    * update HPP notes and add EPI to this repo
    
    * chapter 2 problems
    
    * traversing matrix in spiral order

--- blogs_books_notes/elements_of_programming_interview/chp_2_arrays.py
+++ blogs_books_notes/elements_of_programming_interview/chp_2_arrays.py
@@ -141,8 +141,203 @@ def dutch_flag_partition(A: [], pivot_index: int) -> None:
 """
 5.6 Buy and sell stock once
  
+Write a program that takes an array denoting the daily stock price, and returns the maximum profit that could be made 
+    by buying and then selling one share of that stock. There is no need to buy if no profit is possible.
 
+Hint: Identifying the minimum and maximum is not enough since the minimum may appear after the maximum height.
+    Focus on valid differences.
+"""
+
+test_prices = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+
+
+def max_profit(prices: [float]) -> float:
+    """
+    Q: Can the stock price be negative?
+    A: No
+
+    """
+    # naive solution
+    # Speed: O(n)
+    # Space: O(n)
+    buy = prices[0]
+    cur_sum = 0
+    profits = []
+    for price in prices:
+        if price >= buy:
+            cur_sum += price - buy
+        else:
+            profits.append(cur_sum)
+            cur_sum = 0
+        buy = price
+    profits.append(cur_sum)
+    return max(profits)
+
+
+def max_profit_ben_opt(prices: [float]) -> float:
+    """
+    Optimize it s.t. space complexity is O(1)
+    """
+    buy = prices[0]
+    cur_sum = 0
+    max_pro = 0
+    for price in prices:
+        if price >= buy:
+            cur_sum += price - buy
+        else:
+            if cur_sum > max_pro:
+                max_pro = cur_sum
+            cur_sum = 0
+        buy = price
+    return max_pro
+
+
+def max_profit_book_opt(prices: [float]) -> float:
+    """
+    Optimize it s.t. space complexity is O(1)
+    """
+    min_price_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+    return max_profit
+
+"""
+5.6 variant:
+Write a program that takes an array of integers and finds the length of a longest subarray all of whose entries are 
+    equal. 
 """
 
 
+def len_longest_same_int(ints: [int]) -> int:
+    """
+    O(n), O(1)
+    """
+    max_length, cur_length = 0, 0
+    prev = ints[0]
+    for integer in ints:
+        if integer == prev:
+            cur_length += 1
+            max_length = max(max_length, cur_length)
+        else:
+            prev = integer
+            cur_length = 1
+    return max_length
 
+
+"""
+5.12 Sample Offline Data
+
+Implement an algorithm that takes as input an array of distinct elements and a size, and returns a subset of the given 
+    size of the array elements. All subsets should be equally likely. Return the result in input array itself.
+    
+Hint: How would you construct a random subset of size k + 1 given a random subset of size k?
+"""
+
+import random
+
+
+def _sample(users: [int], k: int) -> None:
+    """
+    * Need to update the input array instead of creating a new one.
+
+    """
+
+    for i in range(k):
+        r_i = random.randint(i, len(users) - 1)
+        users[r_i], users[i] = users[i], users[r_i]
+
+
+"""
+5.18 Compute the spiral ordering of a 2D array
+
+Write a program which takes an n x n 2D array and returns the spiral ordering of the array.
+
+Hint: Use case analysis and divide-and-conquer
+"""
+
+array_2d_3v3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+array_2d_4v4 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+
+
+def spiral_order(square_matrix: [[int]]) -> [int]:
+    """
+    Returns the spiral ordered elements of the square matrix.
+    """
+    # get first and last row. For in between rows, only get first and last element
+    # create list of first + last elements of middle rows + reversed last row + first elements of middle rows
+    # pop the elements and call the function again on the leftover 2d array
+
+    # ben's first failed attempt:
+    # reason: too complicated traverse_spiral function
+    spiral_values = []
+
+    def traverse_spiral(sub_square_matrix: [[int]]):
+        if len(sub_square_matrix) == 1:
+            spiral_values.append(sub_square_matrix[0])
+        n = len(sub_square_matrix) - 2  # index of items that we need per row/column
+        spiral_values.append(sub_square_matrix[0][:n])  # O(n)
+        for row in sub_square_matrix:
+            spiral_values.append(row.pop())
+        spiral_values.append(sub_square_matrix[n + 1][::-1])  # O(n)
+        sub_square_matrix = sub_square_matrix[1:n]  # O(n)
+        new_n = len(sub_square_matrix) - 1
+        while new_n > 0:
+            spiral_values.append(sub_square_matrix[new_n][0])
+            sub_square_matrix[new_n] = sub_square_matrix[new_n][1:]  # O(n)
+            new_n -= 1
+
+        # some sort of operations to redefine the square matrix
+        # some operation to add values to spiral_values
+        # recursive call on the leftover matrix
+        traverse_spiral(sub_square_matrix)
+
+    traverse_spiral(square_matrix)
+    return spiral_values
+
+
+def spiral_order_book_sol1(square_matrix: [[int]]) -> [int]:
+    """
+    Returns the spiral ordered elements of the square matrix (book solution 1)
+
+    <tip> list(zip(*rows)) gives converts lists with rows into lists with columns & vice versa.
+
+    The * character is known as the unpacking operator. when it appears behind an iterable object, what it does is
+        passing the items inside the iterable to the function's caller one by one. In this case, since the zip function
+        accepts a list of iterables in order to return their aligned columns, zip(*p) passes all the items inside the
+        p as arguments to zip function:
+    """
+    # O(n^2)
+    spiral_values = []
+    def traverse_clockwise(offset: int):
+        if offset == len(square_matrix) - offset - 1:
+            # boundary condition. This condition passes if offset >= (len(square_matrix)-1)/2
+            spiral_values.append(square_matrix[offset][offset])
+            return
+        spiral_values.extend(
+            square_matrix[offset][offset:- 1 - offset]
+        )  # first row elements
+        spiral_values.extend(
+            list(zip(*square_matrix))[- 1 - offset][offset:- 1 - offset]
+        )  # last column elements
+        spiral_values.extend(
+            square_matrix[-offset - 1][-1 - offset:offset:-1]
+        )  # last row elements
+        spiral_values.extend(
+            list(zip(*square_matrix))[offset][- 1 - offset: offset: -1]
+        )  # first column elements
+    for offset in range((len(square_matrix) + 1) // 2):
+        traverse_clockwise(offset)
+    return spiral_values
+
+
+def spiral_order_book_sol2(square_matrix: [[int]]) -> [int]:
+    """
+    Returns the spiral ordered elements of the square matrix (book solution 2)
+
+    Try solving this using while loop and two pointers to the end (col_end and row_end):
+    col_start = row_start = 0
+    row_end = len(matrix) - 1
+    col_end = len(matrix[0]) - 1
+    """
